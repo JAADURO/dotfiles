a5a8616f4a5a616bbf9c5f1eb4061deff43e176d
JacynteDurocher <1011806@cstjean.qc.ca>
2024-09-11 11:48:19 -0400

    added dotfiles

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Créer l'arborescence de travail"
+mkdir ~/bin ~/cegep ~/projets
+
+echo "Install virtualbox-guest-utils to manage copy/paste"
+sudo pacman -Sy virtualbox-guest-utils
+
+echo "Installing IDEs"
+sudo pacman -Sy code vim nvim
+
+echo "Installing GNU tools"
+sudo pacman -Sy gcc gbc
+
+echo "Installing Python 3 to enable development and execution of this language"
+sudo pacman -Sy python3 python-pip python-virtualenv
+
+echo "Installing Bash-it for theme"
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it ~/.bash_it/install.sh
